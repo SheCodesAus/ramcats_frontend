0765e8ffce0f49c0fc2278c4152f2a6a9d2bd944
Christin Estrella <estrella.cclm@gmail.com>
2025-01-24 18:11:59 +0800

    navbar fixed

--- furfuturefunding/src/components/NavBar.jsx
+++ furfuturefunding/src/components/NavBar.jsx
@@ -1,18 +1,46 @@
+// NavBar.jsx
+import React, { useState } from 'react';
 import { Link, Outlet } from "react-router-dom";
+import './NavBar.css';
+
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/create-opportunity">Opportunity Form</Link>
-        <Link to="/opportunities">List of Opportunities</Link>
-        {/* I've just created a link to opportunity form so I can see it */}
-      </nav>
-      {/* React Router will pass components into the <Outlet /> based on the path */}
+      <header>
+        <nav>
+          <div className="logo-container">
+            <Link to="/">
+              <img 
+                className="logo-nav"
+                src="src/assets/logo_typography.png"
+                alt="FurFuture Funding Logo" 
+              />
+            </Link> 
+          </div>
+          
+          <button className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
+
+          <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/Login">Login</Link></li>
+              <li><Link to="/About">About</Link></li>
+              <li><Link to="/Contact">Contact</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       <Outlet />
     </div>
   );
